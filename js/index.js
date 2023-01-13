edd8b4574e37c1b5a6b387a9a51f359401c83ecc
Csomi <csomos.adam.benedek@gamil.com>
2023-01-13 06:53:20 +0100

    developer more info

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
 const developerCard = document.getElementById("developer");
 const designerCard = document.getElementById("designer");
 const mediaCard = document.getElementById("media");
+const brandCard = document.getElementById("branding");
 
-// eventlistener if one of the cards is clicked
+// eventlistener if one of the cards is clicked wait 500ms and then redirect to the correct page what is in an other directory
 developerCard.addEventListener("click", () => {
-  setTimeout(() => {
-    window.location.href = "developer.html";
-  }, 500);
+    setTimeout(() => {
+        window.location.href = "developer/";
+    }, 500);
 });
-
 designerCard.addEventListener("click", () => {
-  setTimeout(() => {
-    window.location.href = "designer.html";
-  }, 500);
+    setTimeout(() => {
+        window.location.href = "designer/";
+    }, 500);
 });
-
 mediaCard.addEventListener("click", () => {
-  setTimeout(() => {
-    window.location.href = "media.html";
-  }, 500);
+    setTimeout(() => {
+        window.location.href = "media/";
+    }, 500);
+});
+brandCard.addEventListener("click", () => {
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 500);
 });
