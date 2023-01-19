f4a767fce4cc2051f9cdcdfb5e48c0af23065021
Csomi <csomos.adam.benedek@gamil.com>
2023-01-19 11:11:40 +0100

    music board

--- music/script.js
+++ music/script.js
@@ -2,7 +2,7 @@ const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
   const angle = Math.floor(Math.random() * 20) - 10;
-  const scale = Math.random() * 0.4 + 0.8;
+  const scale = Math.random() * 0.4 + 0.9;
   const x = Math.floor(Math.random() * 40) - 20;
   const y = Math.floor(Math.random() * 40) - 20;
 
@@ -10,3 +10,14 @@ cards.forEach(card => {
   //card.style.transform = `scale(${scale}) translate(${x}px, ${y}px)`;
   
 });
+
+const title = document.querySelector('.title-container');
+const container = document.querySelector('.container');
+
+setTimeout(() => {
+  title.style.opacity = '0'; /* change this line */
+  container.style.display = 'flex';
+  setTimeout(() => {
+    title.style.display = 'none';
+  }, 2000); /* add this line */
+}, 2000);
